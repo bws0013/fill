05f80173006e96ca91a71223e31864e796dd2d2c
Ben <i@auburn.edu>
2017-08-26 13:29:11 -0500

    Added ram taker

---
+++ src/fill_ram/MemoryEater.java
@@ -0,0 +1,20 @@
+/*
+  Copy of program here: https://alvinalexander.com/blog/post/java/java-program-consume-all-memory-ram-on-computer
+*/
+
+import java.util.Vector;
+
+public class MemoryEater
+{
+  public static void main(String[] args)
+  {
+    Vector v = new Vector();
+    while (true)
+    {
+      byte b[] = new byte[1048576];
+      v.add(b);
+      Runtime rt = Runtime.getRuntime();
+      System.out.println( "free memory: " + rt.freeMemory() );
+    }
+  }
+}
